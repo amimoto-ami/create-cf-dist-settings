50872915c7b6b29aa480c206a5db2371aab09efb
wokamoto <wokamoto1973@gmail.com>
2015-12-12 13:52:02 +0900

    added cloudfront-input.json

--- source_dist_setting.sh
+++ source_dist_setting.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source_dist_setting=$(curl -l -s https://gist.githubusercontent.com/wokamoto/08a58534592fead516f7/raw/7c903242229054d5735f75e89709530df2386d5a/cloudfront-input.json \
+source_dist_setting=$(curl -l -s https://raw.githubusercontent.com/amimoto-ami/create-cf-dist-settings/master/cloudfront-input.json \
  | sed "s/%origin-id%/${origin_domain}/g" \
  | sed "s/%caller-reference%/$(date +%s)/g" \
  | sed "s/origin.example.com/${origin_url}/g" \
