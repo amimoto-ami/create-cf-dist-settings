c6f4cc0a812eb4f490cb056dabbe514578c26e44
wokamoto <wokamoto1973@gmail.com>
2016-04-08 16:49:06 +0900

    some fix

--- source_dist_setting.sh
+++ source_dist_setting.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if [ "${domain}" = "" ]; then
+  domain="${origin_domain}"
+fi
 source_dist_setting=$(curl -l -s https://raw.githubusercontent.com/amimoto-ami/create-cf-dist-settings/master/cloudfront-input.json \
  | sed "s/%origin-id%/${domain}/g" \
  | sed "s/%caller-reference%/$(date +%s)/g" \
