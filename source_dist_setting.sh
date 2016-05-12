0fc27cb439561a875984653014063fc2ca1ac650
wokamoto <wokamoto1973@gmail.com>
2016-05-12 17:56:19 +0900

    some fix

--- source_dist_setting.sh
+++ source_dist_setting.sh
@@ -2,7 +2,13 @@
 if [ "${domain}" = "" ]; then
   domain="${origin_domain}"
 fi
-source_dist_setting=$(curl -l -s https://raw.githubusercontent.com/amimoto-ami/create-cf-dist-settings/master/cloudfront-input.json \
+
+json_template='cloudfront-input.json'
+if [ "${domain}" = "" ]; then
+  json_template='cloudfront-input-no-aliases.json'
+fi
+
+source_dist_setting=$(curl -l -s https://raw.githubusercontent.com/amimoto-ami/create-cf-dist-settings/master/${json_template} \
  | sed "s/%origin-id%/${domain}/g" \
  | sed "s/%caller-reference%/$(date +%s)/g" \
  | sed "s/origin.example.com/${origin_url}/g" \
